e61e8919b941844ee538c3acc70e6c18c7dee943
WeiYu53111 <531115357@qq.com>
2023-05-20 15:36:17 +0800

    添加log以及动态设置log级别的接口

--- service/src/index.ts
+++ service/src/index.ts
@@ -6,15 +6,38 @@ import {ChatRoute} from "./routes/ChatRoute";
 import {Request, Response, NextFunction} from 'express';
 import {AuthService} from "./services/AuthService";
 import {CustomRequest} from "./middleware/auth";
-
-import userSevice from "./services/UserService"
+import log4js from 'log4js';
+//import userSevice from "./services/UserService"
 
 //const cors = require('cors')
 const app = express()
 const user = new UserRoute();
 const pub = new PubRoute()
 const chatpgt = new ChatRoute()
-const router = express.Router()
+//const router = express.Router()
+
+const logger = log4js.getLogger('app');
+log4js.configure({
+	appenders: {
+		file: { type: 'file', filename: 'logs/app.log' },
+		console: { type: 'console' }
+	},
+	categories: { default: { appenders: ['file', 'console'], level: 'info' } }
+});
+
+// 将 logger 对象挂载到 app.locals 上
+app.locals.logger = logger;
+
+app.get('/api/logger/level/:level/:pwd', (req: Request, res: Response) => {
+	const { level,pwd } = req.params;
+
+	if(pwd == "Qwe123"){
+		// 获取 logger 对象并更新日志级别
+		const logger = req.app.locals.logger as log4js.Logger;
+		logger.level = level;
+		res.send(`Logger level has been set to ${level}`);
+	}
+});
 
 //app.use(cors())
 app.use(express.static('public'))
@@ -29,19 +52,20 @@ app.all('*', (_, res, next) => {
 // 拦截器中间件函数
 const interceptor = (req: Request, res: Response, next: NextFunction) => {
 	// 在这里编写您的逻辑，例如检查请求头或身份验证等
-	if (req.path.startsWith("/user")) {
-		const params = req.params;
-		const query = req.query;
-		const body = req.body;
-		const path = req.path;
-		console.log({
-			header: req.headers,
-			params: params,
-			query: query,
-			body: body,
-			path: path
-		})
-	}
+	const params = req.params;
+	const query = req.query;
+	const body = req.body;
+	const path = req.path;
+
+	const logger = req.app.locals.logger
+	logger.debug({
+		header: req.headers,
+		params: params,
+		query: query,
+		body: body,
+		path: path
+	})
+		//console.log()
 	// 如果需要终止请求并返回响应，可以像下面这样：
 	// return res.status(401).send('Unauthorized');
 	// 否则，调用next()继续传递请求
@@ -76,8 +100,8 @@ const tokenInterceptor= (req: CustomRequest, res: Response, next: NextFunction) 
 	}
 }
 
-//測試用
-//app.use(interceptor)
+//debug用
+app.use(interceptor)
 
 // 开发模式的时候, vite会重写URL, 这里用不上
 // 部署在nginx的时候 nginx会重写URL, 这里用不上
@@ -91,9 +115,9 @@ app.use('/api/*', (req, res) => {
 app.use(tokenInterceptor);
 
 
-app.use('', router)
+//app.use('', router)
 app.use('/', chatpgt.getRouter())
-app.use('/api', router)
+//app.use('/api', router)
 app.use('/user', user.getRouter());
 app.use('/service', pub.getRouter())
 app.set('trust proxy', 1)
