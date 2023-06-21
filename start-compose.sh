b0d78991ad618893614fc8bd09dfcabc489bd346
WeiYu53111 <531115357@qq.com>
2023-06-21 22:46:07 +0800

    1、优化server dockerfile,镜像体积过大问题 2、优化启动脚本,start与up区分

--- start-compose.sh
+++ start-compose.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 export root_dir=$(pwd)
-if [ "$1" == "start" ]; then
+if [ "$1" == "up" ]; then
     if [ "$2" == "-d" ]; then
         docker compose -f ./deploy/docker-compose/docker-compose.yml up -d
     else
         docker compose -f ./deploy/docker-compose/docker-compose.yml up
     fi
-elif [ "$1" == "stop" ]; then
+elif [ "$1" == "down" ]; then
     docker compose -f ./deploy/docker-compose/docker-compose.yml down
+elif [ "$1" == "start" ]; then
+    docker compose -f ./deploy/docker-compose/docker-compose.yml start
+elif [ "$1" == "stop" ]; then
+    docker compose -f ./deploy/docker-compose/docker-compose.yml stop
 elif [ "$1" == "build" ]; then
     sh ./deploy/build-server.sh && sh ./deploy/build-web.sh
 else
